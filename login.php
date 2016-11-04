0694d68dd7d65467755fd1c1f48d4202cea6a1a6
David Rojas Herrera <david_rojas82142@elpoli.edu.co>
2016-11-03 19:35:27 -0500

    prueba 2 de RDS

--- login.php
+++ login.php
@@ -1,11 +1,11 @@
 <?php
-	$con =  mysqli_connect("mydbinstance.c765otpnmdfd.us-west-2.rds.amazonaws.com", "sebas", "12345", "notijicbd");
+	$con =  mysqli_connect('mydbinstance.c765otpnmdfd.us-west-2.rds.amazonaws.co', 'sebas', '12345', 'notijicbd',3306);
 	$u = $_POST['txtu'];
 	$p = $_POST['txtc'];
-	$q = "select 1 from usuario where correo='$u' and password='$p'";
-	$r = mysqli_query($con,$q);
+  $q = "select 1 from usuario where nombre='$u' and password='$p'";
+  $r = mysqli_query($con,$q);
 	$num = mysqli_num_rows($r);
-	if($num ===0){
+	if($num == 0){
 		print ('Usuario no permirido <a href="index.html">regresar</a>');
 
 	}else{
